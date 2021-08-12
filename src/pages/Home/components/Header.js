ad044915f31514b113ee103e8bf5353375ecc311
João <hlabeats@gmail.com>
2021-08-12 18:20:49 -0300

    initial code

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="Header">
+            <div className="toolbar">
+            <div className="">
+                <span>bluekut</span>
+            </div>
+            <div className="">
+                <button>Nova postagem</button>
+                <span>img1</span>
+                <span>img2</span>
+            </div>
+        </div>
+
+            
+        </header>
+    )
+}
+
+
+export default Header
